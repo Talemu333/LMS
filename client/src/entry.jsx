efe1b09cda360f2aabfe8b55bc688b68e8018e44
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-07 09:20:54 -0700

    Remove administrator frontend routing

--- client/src/entry.jsx
+++ client/src/entry.jsx
@@ -1,9 +1,1 @@
-const path = window.location.pathname;
-
-if (path === '/admin/setup') {
-  import('./admin-setup.jsx');
-} else if (path.startsWith('/admin')) {
-  import('./admin-clean.jsx');
-} else {
-  import('./lms.jsx');
-}
+import './lms.jsx';
