d9c3b2c33c3e00e161cc8f0ddeafd1938701bd18
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-06 18:53:41 -0700

    Use JSX extension for instructor enhancer

--- client/src/entry.jsx
+++ client/src/entry.jsx
@@ -3,5 +3,5 @@ const isAdmin = window.location.pathname.startsWith('/admin');
 if (isAdmin) {
   import('./admin.jsx');
 } else {
-  import('./lms.jsx').then(() => Promise.all([import('./forumEnhancer.js'), import('./instructorEnhancer.js'), import('./studentEnhancer.jsx')]));
+  import('./lms.jsx').then(() => Promise.all([import('./forumEnhancer.js'), import('./instructorEnhancer.jsx'), import('./studentEnhancer.jsx')]));
 }
